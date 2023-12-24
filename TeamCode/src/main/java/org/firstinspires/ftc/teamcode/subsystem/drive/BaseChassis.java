b56ba4707a17aceaeb18e57abd17c9d0000fa181
tree02179 <153999365+tree02179@users.noreply.github.com>
2023-12-24 20:48:35 +0800

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/drive/BaseChassis.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.CusDrive;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.RobotContant;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BaseChassis {
+    public DcMotorEx LF = null;
+    public DcMotorEx RF = null;
+    public DcMotorEx RB = null;
+    public DcMotorEx LB = null;
+    List<DcMotorEx> motors;
+    BaseImu imu;
+
+    public BaseChassis (HardwareMap hardwareMap, boolean chassis) {
+        imu = new BaseImu(hardwareMap);
+        if (chassis) {
+            RobotContant constants = new RobotContant();
+            LF = hardwareMap.get(DcMotorEx.class, constants.LF);
+            RF = hardwareMap.get(DcMotorEx.class, constants.RF);
+            LB = hardwareMap.get(DcMotorEx.class, constants.LB);
+            RB = hardwareMap.get(DcMotorEx.class, constants.RB);
+            motors = Arrays.asList(LF, RF, LB, RB);
+
+            for (DcMotorEx motor: motors) {
+                motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
+
+            LF.setDirection(DcMotorSimple.Direction.REVERSE);
+            LB.setDirection(DcMotorSimple.Direction.REVERSE);
+            RF.setDirection(DcMotorSimple.Direction.FORWARD);
+            RB.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+    }
+
+    public void Drive(Gamepad gamepad) {
+        double y = -gamepad.left_stick_y;
+        double x = gamepad.left_stick_x;
+        double r = gamepad.right_trigger - gamepad.left_trigger;
+
+        LF.setPower(y+x+r);
+        LB.setPower(y-x+r);
+        RF.setPower(y-x-r);
+        RB.setPower(y+x-r);
+    }
+
+    public void AbsMoving(Gamepad gamepad, Telemetry telemetry) {
+        imu.AbsMovingIMU(LF, RF, LB, RB, gamepad, telemetry);
+    }
+}
